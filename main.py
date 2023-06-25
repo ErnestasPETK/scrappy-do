b7a99e15bce69c206fb6a5ba0a50957e82937b59
ErnestasPETK <ernestas.petkevicius53@gmail.com>
2023-06-25 16:15:02 +0300

    fixed click intercepted err

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from confg import Settings
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementClickInterceptedException
 
+import time
 
 
 def main():
@@ -10,12 +12,23 @@ def main():
     driver = webdriver.Chrome()
     print("Starting")
     driver.get(settings.WEATHER_WEB_URL)
-    driver.find_element(by=By.XPATH, value='//*[@id="portlet_weathercurrent_WAR_weatherportlet"]/div/div/div/div[2]/ul/li[1]/a').click()
-    charts = driver.find_elements(by=By.CLASS_NAME, value="chart_box")
+    driver.implicitly_wait(5)
+    hourly_chart_selector = driver.find_element(by=By.XPATH, value='//ul[@class="tabs_nav"]/li/a[contains(text()," 48 hours ")]')
+    if hourly_chart_selector.is_displayed():
+        try: 
+            hourly_chart_selector.click()
+        except ElementClickInterceptedException:
+            time.sleep(5)
+            hourly_chart_selector.click()
+
+    # driver.find_element(by=By.XPATH, value='//div[@class="left"]').click()
+    charts = driver.find_elements(by=By.CLASS_NAME, value="chart-box")
+    print("saving to screenshots")
     for index, chart in enumerate(charts):
         chart.screenshot(f"./screenshot{index}.png")
     print("Chart")
     print(charts)
+    driver.quit()
     print("Ended")
 
 if __name__ == "__main__":
